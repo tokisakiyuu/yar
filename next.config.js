5ac1ce1b8742879ff471a6b9ceae809596234eee
tokisakiyuu <tokisakiyuu@qq.com>
2023-07-06 05:08:24 +0800

    pwa

--- next.config.js
+++ next.config.js
@@ -1,53 +1,8 @@
-const path = require('path')
-const withPWAInit = require('next-pwa')
-
-// Next.js 13 PWA解决方案
-// https://github.com/shadowwalker/next-pwa/issues/424#issuecomment-1332258575
+const withPWANext13 = require('./pwa')
 
 const isDev = process.env.NODE_ENV !== 'production'
 
-const withPWA = withPWAInit({
-  dest: 'public',
-  disable: isDev,
-  exclude: [
-    // add buildExcludes here
-    ({ asset, compilation }) => {
-      if (
-        asset.name.startsWith("server/") ||
-        asset.name.match(/^((app-|^)build-manifest\.json|react-loadable-manifest\.json)$/)
-      ) {
-        return true
-      }
-      if (isDev && !asset.name.startsWith("static/runtime/")) {
-        return true
-      }
-      return false
-    }
-  ]
-})
-
-const generateAppDirEntry = async (entry) => {
-  const registerJs = path.join(path.dirname(require.resolve('next-pwa')), "register.js")
-  const entries = await entry()
-  // Automatically registers the SW and enables certain `next-pwa` features in 
-  // App Router (https://github.com/shadowwalker/next-pwa/pull/427)
-  if (entries["main-app"] && !entries["main-app"].includes(registerJs)) {
-    if (Array.isArray(entries["main-app"])) {
-      entries["main-app"].unshift(registerJs)
-    } else if (typeof entries["main-app"] === "string") {
-      entries["main-app"] = [registerJs, entries["main-app"]]
-    }
-  }
-  return entries
-}
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack(config) {
-    const { entry } = config
-    config.entry = () => generateAppDirEntry(entry)
-    return config
-  }
-}
+const nextConfig = {}
 
-module.exports = withPWA(nextConfig)
+module.exports = isDev ? nextConfig : withPWANext13(nextConfig)
---
+++ pwa.js
@@ -0,0 +1,58 @@
+// Next.js 13 PWA解决方案
+// https://github.com/shadowwalker/next-pwa/issues/424#issuecomment-1332258575
+
+const path = require('path')
+const withPWAInit = require('next-pwa')
+
+const isDev = process.env.NODE_ENV !== 'production'
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  exclude: [
+    // add buildExcludes here
+    ({ asset, compilation }) => {
+      if (
+        asset.name.startsWith("server/") ||
+        asset.name.match(/^((app-|^)build-manifest\.json|react-loadable-manifest\.json)$/)
+      ) {
+        return true
+      }
+      if (isDev && !asset.name.startsWith("static/runtime/")) {
+        return true
+      }
+      return false
+    }
+  ]
+})
+
+const generateAppDirEntry = async (entry) => {
+  const registerJs = path.join(path.dirname(require.resolve('next-pwa')), "register.js")
+  const entries = await entry()
+  // Automatically registers the SW and enables certain `next-pwa` features in 
+  // App Router (https://github.com/shadowwalker/next-pwa/pull/427)
+  if (entries["main-app"] && !entries["main-app"].includes(registerJs)) {
+    if (Array.isArray(entries["main-app"])) {
+      entries["main-app"].unshift(registerJs)
+    } else if (typeof entries["main-app"] === "string") {
+      entries["main-app"] = [registerJs, entries["main-app"]]
+    }
+  }
+  return entries
+}
+
+/**
+ * 
+ * @param {import('next').NextConfig} config 
+ */
+function withPWANext13(config) {
+  return withPWA({
+    webpack(config) {
+      const { entry } = config
+      config.entry = () => generateAppDirEntry(entry)
+      return config
+    },
+    ...config
+  })
+}
+
+module.exports = withPWANext13
