a177772cc7dc2b9b6e4bdcbf76d6f9d20a3c6ee9
tokisakiyuu <tokisakiyuu@qq.com>
2023-07-09 19:32:22 +0800

    authentication system

--- lib/client/store.ts
+++ lib/client/store.ts
@@ -3,11 +3,13 @@ import { getDefaultStore } from 'jotai'
 import storage from './storage'
 import { ExpendRecord } from '../source'
 import { recordsAtom } from '@/app/components/state'
+import { getQueryParameter } from '../utils'
 
 const store = getDefaultStore()
+const apiHeaders = new Headers({ 'Authorization': `Bearer ${getQueryParameter('token')}` })
 
 export async function fetchTable(month: string): Promise<ExpendRecord[]> {
-  const res = await fetch(`/api/table/${month}`)
+  const res = await fetch(`/api/table/${month}`, { headers: apiHeaders })
   if (res.status === 200) {
     const records = await res.json()
     if (dayjs().format('YYYY-MM') === month) {
@@ -24,7 +26,8 @@ export async function updateTable(month: string, records: ExpendRecord[]) {
   }
   await fetch(`/api/table/${month}`, {
     method: 'POST',
-    body: JSON.stringify(records)
+    body: JSON.stringify(records),
+    headers: apiHeaders
   })
 }
 
---
+++ middleware.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const inputToken = getToken(request)
+  if (!inputToken) {
+    return new NextResponse('Authentication required', { status: 401 })
+  }
+  if (inputToken !== process.env.APP_ACCESS_TOKEN) {
+    return new NextResponse('Authentication failed', { status: 401 })
+  }
+  return NextResponse.next()
+}
+
+function getToken(req: NextRequest) {
+  const url = new URL(req.url)
+  const tokenInQuery = url.searchParams.get('token')
+  const authheader = req.headers.get('authorization') || req.headers.get('Authorization') || ''
+  const tokenInHeader = authheader && authheader.split(' ').pop()
+  return tokenInHeader || tokenInQuery || ''
+}
+
+export const config = {
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico).*)'
+  ]
+}
