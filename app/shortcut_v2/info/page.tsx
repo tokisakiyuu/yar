7716ca59f819610cd49ac1a27e215ae8d15a5a48
tokisakiyuu <tokisakiyuu@qq.com>
2024-01-07 17:04:07 +0800

    update link text color

--- app/shortcut_v2/info/page.tsx
+++ app/shortcut_v2/info/page.tsx
@@ -4,18 +4,18 @@ const InfoPage = async () => {
   const records = await kv.zrange("records", 0, -1);
   return (
     <>
-      <div className="">{JSON.stringify(records, null, 2)}</div>
+      <div className="flex">{JSON.stringify(records, null, 2)}</div>
       <div className="mt-2">
-        <a className="p-2 text-blue" href="./clearCachedRecords">
+        <a className="p-2 text-blue-600" href="./clearCachedRecords">
           clear
         </a>
-        <a className="p-2 text-blue" href="./undo">
+        <a className="p-2 text-blue-600" href="./undo">
           undo
         </a>
-        <a className="p-2 text-blue" href="./undoSync">
+        <a className="p-2 text-blue-600" href="./undoSync">
           undoSync
         </a>
-        <a className="p-2 text-blue" href="./sync">
+        <a className="p-2 text-blue-600" href="./sync">
           sync
         </a>
       </div>
