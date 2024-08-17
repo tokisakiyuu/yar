94828d857c12f2dc5e87856075a6aa04fd38cd75
tokisakiyuu <tokisakiyuu@qq.com>
2024-08-17 23:28:35 +0800

    list page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,27 @@
 'use client'
+import useSWR from 'swr'
 
 export default function Home() {
-  return <main>Hello World</main>
+  const { data, isLoading } = useSWR('/api/list?skip=0&limit=99', key =>
+    fetch(key).then(res => res.json()),
+  )
+
+  const records = data?.records ?? []
+  const total = data?.total ?? 0
+  if (isLoading) return 'loading...'
+  if (!records.length) return '(empty)'
+
+  return (
+    <main>
+      <div>
+        {(records as any[]).map(record => (
+          <div>
+            <pre>{JSON.stringify(record, null, 2)}</pre>
+          </div>
+        ))}
+      </div>
+
+      <div>total: {total}</div>
+    </main>
+  )
 }
