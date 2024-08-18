7c60bc28380e7912f0519938a8e7103505695339
tokisakiyuu <tokisakiyuu@qq.com>
2024-08-18 15:11:18 +0800

    api authentication

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,22 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-export function middleware(request: NextRequest) {
-  // const inputToken = getToken(request)
-  // if (!inputToken) {
-  //   return new NextResponse('Authentication required', { status: 401 })
-  // }
-  // if (inputToken !== process.env.APP_ACCESS_TOKEN) {
-  //   return new NextResponse('Authentication failed', { status: 401 })
-  // }
+export function middleware(req: NextRequest) {
+  if (!req.nextUrl.pathname.startsWith('/api')) {
+    return NextResponse.next()
+  }
+
+  if (!isAuthenticated(req)) {
+    return NextResponse.json(
+      { ok: false, msg: 'authentication failed' },
+      { status: 401 },
+    )
+  }
+
   return NextResponse.next()
 }
 
-function getToken(req: NextRequest) {
-  const url = new URL(req.url)
-  const tokenInQuery = url.searchParams.get('token')
-  const authheader =
-    req.headers.get('authorization') || req.headers.get('Authorization') || ''
-  const tokenInHeader = authheader && authheader.split(' ').pop()
-  return tokenInHeader || tokenInQuery || ''
+function isAuthenticated(req: NextRequest) {
+  const { headers } = req
+  const value = headers.get('authorization')
+  return value === `token ${process.env.APP_ACCESS_TOKEN}`
 }
-
-export const config = {
-  matcher: '/api/:function*',
-}
-
