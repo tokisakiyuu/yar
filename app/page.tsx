47f0cd6f5fa57426c454839a95ff0179c2cbcdad
tokisakiyuu <tokisakiyuu@qq.com>
2024-08-18 15:55:45 +0800

    page authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,39 @@
 'use client'
+import { useEffect } from 'react'
+import { useLocalStorage, useSearchParam } from 'react-use'
 import useSWR from 'swr'
 
 export default function Home() {
-  const { data, isLoading } = useSWR('/api/list?skip=0&limit=99', key =>
-    fetch(key).then(res => res.json()),
+  const token = useToken()
+  const { data, isLoading, error } = useSWR(
+    '/api/list?skip=0&limit=99',
+    async key => {
+      const res = await fetch(key, {
+        headers: {
+          Authorization: `token ${token}`,
+        },
+      })
+      if (res.status === 200) {
+        return res.json()
+      } else {
+        throw res.statusText
+      }
+    },
   )
 
   const records = data?.records ?? []
   const total = data?.total ?? 0
+
   if (isLoading) return 'loading...'
+  if (error) return <pre style={{ color: 'red' }}>{error}</pre>
+
   if (!records.length) return '(empty)'
 
   return (
     <main>
       <div>
         {(records as any[]).map(record => (
-          <div>
+          <div key={record._id}>
             <pre>{JSON.stringify(record, null, 2)}</pre>
           </div>
         ))}
@@ -24,4 +42,17 @@ export default function Home() {
       <div>total: {total}</div>
     </main>
   )
+}
+
+function useToken() {
+  const incomeToken = useSearchParam('token')
+  const [token, setToken] = useLocalStorage<string>('token')
+
+  useEffect(() => {
+    if (typeof incomeToken === 'string') {
+      setToken(incomeToken)
+    }
+  }, [incomeToken, token])
+
+  return incomeToken || token
 }
